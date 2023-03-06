16a784e644265ccd47bfa8482631f57c80ca7f98
Tony Collett <tony@jaix.com.au>
2023-03-06 16:00:17 +1030

    prep for user & password

--- BlepItLibrary/Models/User.cs
+++ BlepItLibrary/Models/User.cs
@@ -6,6 +6,7 @@ public class User
     public string Id { get; set; }
     public string ObjectIdentifier { get; set; }
     public string Username { get; set; }
+    public string Password { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string DisplayName => FirstName + " " + LastName;
